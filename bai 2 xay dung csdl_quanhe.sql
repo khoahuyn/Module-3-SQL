a22dcd625bbbf8368a7c2ab7626453465bcb4ce0
khoahuyn <khoahuynh2k3hi@gmail.com>
2023-03-15 20:15:35 +0700

    update

--- bai 2 xay dung csdl_quanhe.sql
+++ bai 2 xay dung csdl_quanhe.sql
@@ -1,1 +1,30 @@
 create database QUANLY_BANHANG;
+
+create table Customer(
+	cID int Not null auto_increment primary key,
+    cName varchar(50) unique,
+    cAge int Not null
+);
+
+create table ORDERS(
+	oID int Not null auto_increment primary key,
+	cID int Not null ,
+    foreign key(cID)references Customer(cID),
+    oDate varchar(50) unique,
+    oTotalPrice int Not null
+);
+
+
+create table PRODUCT(
+	pID int Not null auto_increment primary key,
+    pName varchar(50) unique,
+    pPrice int Not null
+);
+
+create table ORDERDETAIL(
+	oID int Not null ,
+	pID int Not null ,
+    odQTY int not null,
+	foreign key(oID) references ORDERS(oID),
+	foreign key(pID) references PRODUCT(pID)
+);
